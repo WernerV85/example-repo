7bcd8e56b193d28427376a1a7d85b8d9fb9927f8
WernerVal-8485 <wernerval00@gmail.com>
2025-05-07 20:19:57 +0200

    Create hello_world.py

---
+++ Level 1 - Python for Software Engineering/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,15 @@
+# ------------pseudocode-------
+#create variable named "user_name"
+#request user input to supply name
+#print variable "user_name"
+#create variable "user_age"
+#request user input on user's age
+#print variable "user_age"
+#print text "Hello, World!"
+
+
+user_name = input("What is your name: ")
+print(user_name)
+user_age = input("What is you age, " + user_name + ": ")
+print(user_age)
+print("Hellow, World!")
