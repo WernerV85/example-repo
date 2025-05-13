ce668ea327ff396e3967763b400887ac5fef7104
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 14:43:04 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
@@ -5,7 +5,7 @@ last_char = str_manip[-1]
 print(last_char)
 
 str_manip = str_manip.replace(last_char, "@")
-print(f"str_manip.replace(): {str_manip}")
+print( {str_manip} )
 
 print(str_manip[len(str_manip):len(str_manip)-4:-1])
 
