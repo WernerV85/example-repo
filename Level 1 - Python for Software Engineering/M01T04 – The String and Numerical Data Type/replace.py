401841baf9d3ebc2b934b6405198ff42636b593b
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 17:27:21 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/replace.py
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/replace.py
@@ -7,12 +7,12 @@ save_sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
 #create replace function and charcters to replace
 save_sentence = save_sentence.replace("!", " ")
 #print function
-print(f"save_sentence.replace(): {save_sentence}")
+print({save_sentence})
 
 
 #***Print as uppercase**
 save_sentence = save_sentence.upper()
-print(f"save_Sentance.upper(): {save_sentence}")
+print({save_sentence})
 
 #printing sentence in reverse
 print(save_sentence[::-1])
