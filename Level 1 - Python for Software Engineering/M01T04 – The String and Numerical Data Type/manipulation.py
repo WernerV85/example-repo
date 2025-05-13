7e9f6a5558e74c801611623a86660c8a07a79dda
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 13:38:50 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
@@ -3,3 +3,6 @@ print(f"Length of sentence is {len(str_manip)} characters")
 
 last_char = str_manip[-1]
 print(last_char)
+
+str_manip = str_manip.replace(last_char, "@")
+print(f"str_manip.replace(): {str_manip}")
