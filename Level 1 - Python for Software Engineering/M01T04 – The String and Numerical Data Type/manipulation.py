008b2b74c80194f358f668a492fecf5f9eedac12
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 17:25:22 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
@@ -1,12 +1,25 @@
+#-----Auto grade task2
+
+#String Manipulation
+
+#set variable as well as request input from user for sentence
 str_manip = input("Please supply any sentence: ")
+#print the lenght of the input sentence from user
 print(f"Length of sentence is {len(str_manip)} characters")
 
+#Identify last character in sentence
+#create variable for last character for later use
 last_char = str_manip[-1]
+#print last character
 print(last_char)
 
+#replacing last character with @ through the whole sentence
 str_manip = str_manip.replace(last_char, "@")
+#printing new sentence with replacement in place
 print( {str_manip} )
 
+#printing the last 3 letters of the sentence
 print(str_manip[len(str_manip):len(str_manip)-4:-1])
 
+#printing the first 3 and last 2 letters of the sentence to create a new word
 print(str_manip[0 : 3 : 1] + str_manip[len(str_manip)-2 : len(str_manip) : 1])
