ea62539168a22c7b1bf990ddc8426fda32183d49
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 20:27:13 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T05 – Control Structures – If, Elif, Else and the Boolean Data Type/age-quiz.py
+++ Level 1 - Python for Software Engineering/M01T05 – Control Structures – If, Elif, Else and the Boolean Data Type/age-quiz.py
@@ -4,7 +4,7 @@ if age_user > 100:
     print("Sorry, your're dead!")
 elif age_user >= 65 and age_user <= 100:
     print("Enjoy your retirement!")
-elif age_user <= 40 and age_user < 65:
+elif age_user >= 40 and age_user < 65:
     print("You're over the hill!")
 elif age_user == 21:
     print("Congrats on your 21st")
