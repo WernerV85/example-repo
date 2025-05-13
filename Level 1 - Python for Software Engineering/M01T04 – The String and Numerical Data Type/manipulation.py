b19bf40dbdf28ec5a0db982a74b9629687768827
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 13:16:53 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/manipulation.py
@@ -2,4 +2,5 @@ str_manip = input("Please supply any sentance: ")
 print(f"Length of sentence is {len(str_manip)} characters")
 
 final_char = len(str_manip)
-print(final_char)
+
+print(final_char(len(str_manip)))
