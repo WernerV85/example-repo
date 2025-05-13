87ad84ba7fcb5bd98dbc0f05f61a9ff9f7685b34
WernerVal-8485 <wernerval00@gmail.com>
2025-05-13 17:42:33 +0200

    commit

---
+++ Level 1 - Python for Software Engineering/M01T04 – The String and Numerical Data Type/integermath.py
@@ -0,0 +1,17 @@
+num1 = int(input("Please supply the first of three numbers: "))
+num2 = int(input("Please supply the second of three numbers: "))
+num3 = int(input("Please give the third and last of the three numbers: "))
+
+print(f"The number you selected are {num1} , {num2} and {num3} .")
+
+cal1 = sum(num1, num2, num3)
+print(sum1)
+
+cal2 = num1 - num2
+print(cal2)
+
+cal3 = num3 * num1
+print(cal3)
+
+cal4 = cal1 / num3
+print(cal4)
