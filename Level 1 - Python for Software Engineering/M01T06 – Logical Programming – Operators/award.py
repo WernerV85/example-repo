e84579d724cd989743df36e22267e66fce95d531
WernerVal-8485 <wernerval00@gmail.com>
2025-05-14 18:18:50 +0200

    commit

---
+++ Level 1 - Python for Software Engineering/M01T06 – Logical Programming – Operators/award.py
@@ -0,0 +1,9 @@
+swim_time = int(input("Please give your time (in minutes) for the Swimming section of the Triathlon: "))
+cycle_time = int(input("Please give your time (in minutes) for the Cycling section of the Triathlon: "))
+run_time = int(input("Please give your time (in minutes) for the Running section of the Triathlon: "))
+
+total_time = swim_time + cycle_time + run_time
+
+print(f"Total time to complete the Triathlon: {total_time} minutes")
+
+
