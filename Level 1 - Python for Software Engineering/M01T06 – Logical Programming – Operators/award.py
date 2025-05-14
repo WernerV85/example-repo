0d64505c1ae755f27359a6844d56e1af86917336
WernerVal-8485 <wernerval00@gmail.com>
2025-05-14 18:26:02 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T06 – Logical Programming – Operators/award.py
+++ Level 1 - Python for Software Engineering/M01T06 – Logical Programming – Operators/award.py
@@ -6,4 +6,12 @@ total_time = swim_time + cycle_time + run_time
 
 print(f"Total time to complete the Triathlon: {total_time} minutes")
 
-
+if total_time >= 0 or total_time <= 100:
+    print("Award: Provincial Colours.")
+elif total_time >= 101 and total_time <= 105:
+    print("Award: Provincial Half Coulours.")
+elif total_time >= 106 and total_time <= 110:
+    print("Award: Provincial Scroll.")
+else:
+    print("No Award!")
+    
