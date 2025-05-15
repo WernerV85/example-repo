c2b72c348842d57a4e0a6380ef389b05f29e7c8b
WernerVal-8485 <wernerval00@gmail.com>
2025-05-15 19:48:17 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
+++ Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
@@ -6,7 +6,7 @@ Bond       - to calcuate the amount you'll have to pay on a home loan.
 user_choice = str(input('''Enter either "Investment" or "Bond" form the menu above to proceed: '''))
 user_choice = user_choice.upper
 print(user_choice)
-if user_choice.upper == "INVESTMENT":
+if user_choice == "INVESTMENT":
     dep_amount = float(input("Please supply the amount of money being deposited: "))
     interest_rate = float(input('''What is the interest rate (percentage) at which the money is being deposited: 
                                 (please only give the number, exclude the % symbol)'''))
