58a9ae7cf721d11e4e4bc496bc6bcefd573e98ab
WernerVal-8485 <wernerval00@gmail.com>
2025-05-15 19:49:16 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
+++ Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
@@ -4,7 +4,7 @@ print('''Investment - to calculate the amount on interest you'll earn on your in
 Bond       - to calcuate the amount you'll have to pay on a home loan. 
       ''')
 user_choice = str(input('''Enter either "Investment" or "Bond" form the menu above to proceed: '''))
-user_choice = user_choice.upper
+user_choice = user_choice.upper()
 print(user_choice)
 if user_choice == "INVESTMENT":
     dep_amount = float(input("Please supply the amount of money being deposited: "))
