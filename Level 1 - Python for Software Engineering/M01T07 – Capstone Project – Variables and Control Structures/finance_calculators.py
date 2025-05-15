731a38ca96f010a10e8cf6c0bed4f1efdda86223
WernerVal-8485 <wernerval00@gmail.com>
2025-05-15 21:36:17 +0200

    final commit

--- Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
+++ Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
@@ -10,17 +10,17 @@ Bond       - to calcuate the amount you'll have to pay on a home loan. ''')
 #Define first variable user choice to select what calculation they want to calculate.
 user_choice = str(input('''Enter either "Investment" or "Bond" form the menu above to proceed: '''))
 
-#This part was tricky, my first thought was to populate a list with all the different types the user may input
-#As I work for a software company, I had a chat regarding this assignment with one of our developers
-#I told him about my initial plans, and he suggested the conversion to uppercase
-#I took the day and thought this over and decided to use his suggestion
-#As he only gave me the suggestion I am not going to reference him as he did not help me with the code, only the idea and it was verbal
+    ##This part was tricky, my first thought was to populate a list with all the different types the user may input
+    ##As I work for a software company, I had a chat regarding this assignment with one of our developers
+    ##I told him about my initial plans, and he suggested the conversion to uppercase
+    ##I took the day and thought this over and decided to use his suggestion
+    ##As he only gave me the suggestion I am not going to reference him as he did not help me with the code, only the idea and it was verbal
 user_choice = user_choice.upper()
 
 #If statement start for if user select Investment
 if user_choice == "INVESTMENT":
 
-    #Request all inputs from user
+    #Request all inputs from user as instructed
     dep_amount = float(input("Please supply the amount of money being deposited (H$): "))
 
     interest_rate = float(input('''What is the interest rate (percentage) at which the money is being deposited 
@@ -41,30 +41,72 @@ if user_choice == "INVESTMENT":
 
     #If statement to calculate Simple investment interest
     if interest == "SIMPLE":
+        #used calculation as setout in the reading material
+            ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
         calculate_simple = dep_amount * (1 + interest_percen * invest_year)
         #Used the rounding function to show final result as 2 decimals or cents
-        #Hyoerion 10-006 The String and Numerical Data Types, page 10
+            #HyperionDev, 10-006 The String and Numerical Data Types, page 10, copyright 2025 HyperionDev
         calculate_simple = round(calculate_simple,2)
+
+        #print text to inform user of how much interst will be earned, used H$ as in example in Capstone reading material
+            ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
+        #Thousand seperator was used to make the reading of the value even more user friendly, got the syntax from W3school website
+            ##https://www.w3schools.com/python/python_operators.asp, W3Schools - WE.CSS. , copyright 1999 - 2025 by Refsnes Data
         print(f"The interest you will earn over {invest_year} years is equal to H$ {calculate_simple:,}!")
+
+    #second part (elif) to calculate the compound interest
     elif interest == "COMPOUND":
+
+        #used calculation as setout in the reading material
+            ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
         calculate_compound = dep_amount * math.pow((1 + interest_percen), invest_year)
+
+        #Used the rounding function to show final result as 2 decimals or cents
+            ##HyperionDev, 10-006 The String and Numerical Data Types, page 10, copyright 2025 HyperionDev
         calculate_compound = round(calculate_compound,2)
+
+        #print text to inform user of how much interst will be earned, used H$ as in example in Capstone reading material
+            ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
+        #Thousand seperator was used to make the reading of the value even more user friendly, got the syntax from W3school website
+            ##https://www.w3schools.com/python/python_operators.asp, W3Schools - WE.CSS. , copyright 1999 - 2025 by Refsnes Data
         print(f"The interest you will earn over {invest_year} years is equal to H$ {calculate_compound:,}!")
+
+        #Created a 'end' or else statement if the user entered any other selection as stated in the instruction in line 36
     else :
         print("Incorrect selection!! Please return to Main Menu!!")
 
+
+#Second part/ elif statement for if bond was selected in the first input in line 11
 elif user_choice == "BOND":
+
+    #requested input from user as instructed in reading material
+     #used H$ as in example in Capstone reading material
+        ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
     house_value = float(input("What is the value of the house (H$): "))
+
     house_interest_rate = float(input('''What is the interest rate (percentage) of the bond
     (please only give the value, exclude the % symbol): '''))
+    #calculated the interest rate per month to be used in final calculation
     house_month_interest = (house_interest_rate / 100) / 12
+
     num_months = int(input('''Bond period - Months
     (In how many months will the bond be paid off?): '''))
-
+    
+    #used calculation as setout in the reading material
+         ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
     monthly_repay = (house_month_interest * house_value) / (1 - (1 + house_month_interest) ** (-num_months))
+
+    #Used the rounding function to show final result as 2 decimals or cents
+        ##HyperionDev, 10-006 The String and Numerical Data Types, page 10, copyright 2025 HyperionDev
     monthly_repay = round(monthly_repay,2)
+
+    #print text to inform user of how much the bond repayment per month will be, used H$ as in example in Capstone reading material
+        ##HyperionDev, M01T07 – Capstone Project – Variables and Control Structures, page 3, copyright 2025 HyperionDev
+    #Thousand seperator was used to make the reading of the value even more user friendly, got the syntax from W3school website
+        ##https://www.w3schools.com/python/python_operators.asp, W3Schools - WE.CSS. , copyright 1999 - 2025 by Refsnes Data
     print(f"The monthly Bond repayment is equal to H$ {monthly_repay:,}!")
 
+#Created a 'end' or else statement if the user entered any other selection as stated in the instruction in line 11
 else :
     print("Incorrect selection!! Please return to Main Menu!!")
 
