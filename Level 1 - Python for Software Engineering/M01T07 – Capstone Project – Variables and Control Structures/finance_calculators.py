d232cee67db9302c306006541a758b04bc1347de
WernerVal-8485 <wernerval00@gmail.com>
2025-05-15 20:51:33 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
+++ Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
@@ -8,7 +8,7 @@ user_choice = str(input('''Enter either "Investment" or "Bond" form the menu abo
 user_choice = user_choice.upper()
 
 if user_choice == "INVESTMENT":
-    dep_amount = float(input("Please supply the amount of money being deposited: "))
+    dep_amount = float(input("Please supply the amount of money being deposited (H$): "))
     interest_rate = float(input('''What is the interest rate (percentage) at which the money is being deposited 
     (please only give the value, exclude the % symbol): '''))
     interest_percen = interest_rate / 100
@@ -21,16 +21,16 @@ if user_choice == "INVESTMENT":
     if interest == "SIMPLE":
         calculate_simple = dep_amount * (1 + interest_percen * invest_year)
         calculate_simple = round(calculate_simple,2)
-        print(f"The interest you will earn over {invest_year} years is equal to {calculate_simple}!")
+        print(f"The interest you will earn over {invest_year} years is equal to H$ {calculate_simple:,}!")
     elif interest == "COMPOUND":
         calculate_compound = dep_amount * math.pow((1 + interest_percen), invest_year)
         calculate_compound = round(calculate_compound,2)
-        print(f"The interest you will earn over {invest_year} years is equal to {calculate_compound}!")
+        print(f"The interest you will earn over {invest_year} years is equal to H$ {calculate_compound:,}!")
     else :
         print("Incorrect selection!! Please return to Main Menu!!")
 
 elif user_choice == "BOND":
-    house_value = float(input("What is the value of the house: "))
+    house_value = float(input("What is the value of the house (H$): "))
     house_interest_rate = float(input('''What is the interest rate (percentage) of the bond
     (please only give the value, exclude the % symbol): '''))
     house_month_interest = (house_interest_rate / 100) / 12
@@ -39,7 +39,7 @@ elif user_choice == "BOND":
 
     monthly_repay = (house_month_interest * house_value) / (1 - (1 + house_month_interest) ** (-num_months))
     monthly_repay = round(monthly_repay,2)
-    print(f"The monthly Bond repayment is equal to {monthly_repay}!")
+    print(f"The monthly Bond repayment is equal to H$ {monthly_repay:,}!")
 
 else :
     print("Incorrect selection!! Please return to Main Menu!!")
