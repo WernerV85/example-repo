502ecd06b49251f8713dd924e4ee406067061ebd
WernerVal-8485 <wernerval00@gmail.com>
2025-05-15 19:30:38 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
+++ Level 1 - Python for Software Engineering/M01T07 – Capstone Project – Variables and Control Structures/finance_calculators.py
@@ -1,7 +1,7 @@
 import math
 
 print('''Investment - to calculate the amount on interest you'll earn on your investment
-Bond        - to calcuate the amount you'll have to pay on a home loan. ''')
+Bond      - to calcuate the amount you'll have to pay on a home loan. ''')
 user_choice = str(input('''Enter either "Investment" or "Bond" form the menu above to proceed: '''))
 user_choice = upper(user_choice)
 print(user_choice)
