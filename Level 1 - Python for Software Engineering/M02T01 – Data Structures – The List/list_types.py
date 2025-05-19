7661f1ef80142a794d5bf067361c57c90d2ce0dd
WernerVal-8485 <wernerval00@gmail.com>
2025-05-19 20:46:51 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T01 – Data Structures – The List/list_types.py
+++ Level 1 - Python for Software Engineering/M02T01 – Data Structures – The List/list_types.py
@@ -4,3 +4,16 @@
 #create a list of 3 friend's names
 friends_names = ["Peter" , "Brain" , "Edd"]
 print(friends_names)
+
+#task 2 - Printing first and last names of friends
+print(friends_names[0])
+print(friends_names[-1])
+print(len(friends_names))
+
+#task 3 - define new list
+friends_ages = [12 , 25, 50]
+
+#task 4 - Print each friends name and age
+print(f'My friend {friends_names[0]} is {friends_ages[0]} years old.')
+print(f'My other {friends_names[1]} is {friends_ages[1]} years old.')
+print(f'My last {friends_names[2]} is {friends_ages[2]} years old, he is the oldest.')
