c510ef9e6be18277af0dbc9a43a2904ce1467d3b
WernerVal-8485 <wernerval00@gmail.com>
2025-05-20 20:18:47 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -1,6 +1,6 @@
 num_list = []
 num_input = 0
-#num_input = int(input(f"Please give any number: "))
+num_input = int(input(f"Please give any number: "))
 while num_input > 0 :
     num_input = int(input(f"Please give any number: "))
     num_input += num_input
@@ -10,4 +10,4 @@ while num_input > 0 :
     #print(num_input)
     #print(num_list)
     num_ave = num_input / len(num_list)
-    print(num_ave)
+    print(float(num_ave))
