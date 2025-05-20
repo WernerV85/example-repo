bdd2a817e993cb40b892f7b4b1998975f49cbf2f
WernerVal-8485 <wernerval00@gmail.com>
2025-05-20 21:04:30 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -9,8 +9,9 @@ while num_input > 0:
     
     
     if num_input == -1:
-        num_ave = num_input / num_list
+        break
+    num_ave = num_input / num_list
     #print(num_input)
    # print(num_list)
-        print(num_ave)
+    print(num_ave)
     
