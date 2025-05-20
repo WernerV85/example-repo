075ba84feaceed05b957d461bf0ba11d0a1c1f7d
WernerVal-8485 <wernerval00@gmail.com>
2025-05-20 21:34:35 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -11,6 +11,7 @@ while num_input >= 0:
     #print(f"The count of the values are: {num_list}")
     if num_input == -1:
         print(f"The sum of the values are: {num_total}")
+        print(f"The count of the values are: {num_list}")
         num_ave = num_total / num_list
         print(f"Average of the numbers entered is: {num_ave}")
     
