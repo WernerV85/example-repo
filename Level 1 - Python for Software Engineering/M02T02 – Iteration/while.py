4f045946129e945b7532a98c3715e062e43a5f0b
WernerVal-8485 <wernerval00@gmail.com>
2025-05-20 20:56:59 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -5,7 +5,7 @@ num_input = int(input(f"Please give any number: "))
 while num_input > 0:
     num_input = int(input(f"Please give any number: "))
     num_input += num_input
-    num_list.append(num_input)
+    num_list +=1
     
     
     if num_input == -1:
