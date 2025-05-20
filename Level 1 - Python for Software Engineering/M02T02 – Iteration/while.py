d5e07794f9786325c79ac2b66e2b2e51fb11eb05
WernerVal-8485 <wernerval00@gmail.com>
2025-05-20 18:30:39 +0200

    commit

---
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -0,0 +1,7 @@
+num_list = []
+num_input = int(input(f"Please give any number: "))
+while num_input is not 0 :
+    num_input = num_list
+    if num_input == -1:
+        num_ave = numlist / len(num_list)
+        print(num_ave)
