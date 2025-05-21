21a5ba63182a7bdb99a52534a559f58aacf135c8
WernerVal-8485 <wernerval00@gmail.com>
2025-05-21 07:04:30 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -1,21 +1,21 @@
 num_input = 1
-num_list = 0
+num_count = 0
 num_total = 0
 
 
 while num_input >= 1:
     num_input = int(input(f"Please give any number: "))
     num_total += num_input
-    num_list +=1
+    num_count +=1
 
     if num_input == -1:
         num_total += 1
         num_list -= 1
         print(f"The sum of the values are: {num_total}")
-        print(f"The count of the values are: {num_list}")
-        num_ave = num_total / num_list
+        print(f"The count of the values you've entered is: {num_count}")
+        num_ave = num_total / num_count
         print(f"Average of the numbers entered is: {num_ave}")
-        
+
     elif num_input == 0:
         print(f"Invalid input")
 
