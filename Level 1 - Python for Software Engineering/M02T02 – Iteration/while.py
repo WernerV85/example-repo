e28771e329e63b48cdc52f247ee5e16bc82a5b2b
WernerVal-8485 <wernerval00@gmail.com>
2025-05-21 06:55:35 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/while.py
@@ -1,16 +1,16 @@
-num_input = 0
+num_input = 1
 num_list = 0
 num_total = 0
 
 #num_input = int(input(f"Please give any number: "))
-while num_input > 0:
+while num_input >= 1:
     num_input = int(input(f"Please give any number: "))
     num_total += num_input
     num_list +=1
     #print(f"The sum of the values are: {num_total}")
     #print(f"The count of the values are: {num_list}")
     if num_input == -1:
-        num_total += 1
+        #num_total += 1
         num_list -= 1
         print(f"The sum of the values are: {num_total}")
         print(f"The count of the values are: {num_list}")
