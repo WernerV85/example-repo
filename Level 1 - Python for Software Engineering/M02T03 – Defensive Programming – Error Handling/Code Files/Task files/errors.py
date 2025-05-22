3bafd1d8fa8b7023b6cdf0cfafad40a8c9cf51b6
WernerVal-8485 <wernerval00@gmail.com>
2025-05-22 19:17:59 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors.py
+++ Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors.py
@@ -2,23 +2,33 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-print "Welcome to the error program"
-    print "\n"
+print ("Welcome to the error program") 
+#Syntax error - No parentheses
+print ("\n") 
+#Syntax error - incorrect indentation
 
     # Variables declaring the user's age, casting the str to an int, and printing the result
-    age_Str == "24 years old" 
-    age = int(age_Str) 
-    print("I'm" + age + "years old.")
+age_Str = "24" 
+#Syntax error - incorrect indentation also incorrect usage of ==
+age = int(age_Str) 
+#Syntax error - incorrect indentation
+print(f"I'm {age} years old.")  
+#Syntax error - incorrect indentation also incorrect usage of f print
 
     # Variables declaring additional years and printing the total years of age
-    years_from_now = "3"
-    total_years = age + years_from_now
+years_from_now = 3.5  
+#Syntax error - incorrect indentation Logical error - variable set incorrectly as str
+total_years = age + years_from_now  
+#Syntax error - incorrect indentation
 
-print "The total number of years:" + "answer_years"
+print (f"The total number of years: {total_years}")  
+#Syntax error - No parentheses , Wrong "" and incorrect variable
 
 # Variable to calculate the total number of months from the given number of years and printing the result
-total_months = total * 12
-print "In 3 years and 6 months, I'll be " + total_months + " months old"
+total_months = int(total_years * 12) 
+#Logical error, spelling mistake
+print (f"In 3 years and 6 months, I'll be {total_months} months old")  
+#Syntax error - No parentheses #Syntax error - incorrect indentation also incorrect usage of f print
 
 #HINT, 330 months is the correct answer
 
