e52cf7cdfc7b554ab84fe2b36c635561c4ff7dc7
WernerVal-8485 <wernerval00@gmail.com>
2025-05-22 18:22:51 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T02 – Iteration/pattern.py
+++ Level 1 - Python for Software Engineering/M02T02 – Iteration/pattern.py
@@ -9,11 +9,11 @@ count_num = 0
 # Start for loop with inbedded if statement
 # if statement is to look for the range of value
 # if range reaches 5 count down stated
-for i in range (0, 8):
+for i in range (0, 9):
     if i == 0 or i < 5:
         i += 1
         count_num = i
         print("*" * count_num)
-    elif i == 5 or i < 8:
+    elif i == 5 or i < 9:
         count_num -= 1
         print("*" * count_num)
