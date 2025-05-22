4d399d96a090705a840c636f7270658680229af2
WernerVal-8485 <wernerval00@gmail.com>
2025-05-22 20:31:37 +0200

    commit

---
+++ Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/logic.py
@@ -0,0 +1,18 @@
+## Auto-graded task 2
+
+print(''' Auto-graded task 2
+      
+      
+      ''')
+
+name_user = "Werner Valentin"
+user_age = 39
+user_height = 1.85
+user_weight = "125.5 kg"
+
+print(f'''Hi, My name is {user_age}, 
+I am {user_height} years old,
+my current weight is {name_user}
+and I am {user_height} meter in height!!
+It is nice to meet you,
+you can call me {name_user[0 : 6]}. ''')
