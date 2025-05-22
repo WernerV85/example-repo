27f70416f38e3aa166e8b00104f37ddf0e4a8675
WernerVal-8485 <wernerval00@gmail.com>
2025-05-22 19:25:14 +0200

    commit

--- Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
+++ Level 1 - Python for Software Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
@@ -2,11 +2,15 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-animal = Lion
+animal = "Lion" 
+# Logical error - Str not defines by ""
 animal_type = "cub"
 number_of_teeth = 16
 
-full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+full_spec = (f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth") 
+#Syntax error - print funtion missing () and incorrect missing f string syntax
+# Logical error - wrong variable inserted in the incorrect place in the sentence
 
-print full_spec
+print (full_spec) 
+# Syntax error - print function missing ()
 
