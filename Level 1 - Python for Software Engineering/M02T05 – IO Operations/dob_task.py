155cc6ff6d2b99aceeab11bd54eff4912360bb89
WernerVal-8485 <wernerval00@gmail.com>
2025-05-27 21:11:37 +0200

    commit final 20250527

--- Level 1 - Python for Software Engineering/M02T05 – IO Operations/dob_task.py
+++ Level 1 - Python for Software Engineering/M02T05 – IO Operations/dob_task.py
@@ -1,8 +1,17 @@
+## Auto-graded task - 10-039IO Operations
+
+print(''' Auto-graded tast 1
+      
+      ''')
+
+# Define empty lists to store content
 name_list = []
 date_list = []
 
 print("Name: ")
 
+# Open file and split the list into individual words
+# Create new list to print concatinating first two word in list
 with open('Level 1 - Python for Software Engineering\M02T05 – IO Operations\Code Files\Input\Task file\DOB.txt', 'r') as file:
     for lines in file:
         name_list = lines.split(" ")
@@ -13,6 +22,8 @@ print('''
       
 Birthdate: ''')   
 
+# Open file and split list into individual words
+# Create new list to print by concatinating the last 3 word
 with open('Level 1 - Python for Software Engineering\M02T05 – IO Operations\Code Files\Input\Task file\DOB.txt', 'r') as d_file1:
     for lines in d_file1:
         date_list = lines.split(" ")
