9d8776d02552cc88a038145b51282a0a683c11ae
WernerVal-8485 <wernerval00@gmail.com>
2025-05-28 19:53:16 +0200

    commit 20250528

--- Level 1 - Python for Software Engineering/M02T05 – IO Operations/student_register.py
+++ Level 1 - Python for Software Engineering/M02T05 – IO Operations/student_register.py
@@ -1,17 +1,13 @@
-#path = "C:\\Users\\WernerV\\Documents\\GitHub\\WV25050018135\\Level 1 - Python for Software Engineering\\M02T05 – IO Operations\\reg_form.py"
-student_s = ""
+
+student_s = []
+student_number = []
 
 student_count = int(input(f"Please give the number of students that will write exam: "))
 
-for line in student_count:
-    student_number = input(f"Please enter student's number for attendance register: ")
-    lines.append(line)
-print(lines)
 
-with open("C:\\Users\\WernerV\\Documents\\GitHub\\WV25050018135\\Level 1 - Python for Software Engineering\\M02T05 – IO Operations\\reg_form.py" , "a+") as file:
-    for line in file:
-        for lines in student_count:
-            student_number = input(f"Please enter student's number for attendance register: ")
-            lines.append(line)
-            file.write(student_number + "____________" + "\n")
-        print(lines)
+with open('C:\\Users\\WernerV\\Documents\\GitHub\\WV25050018135\\Level 1 - Python for Software Engineering\\M02T05 – IO Operations\\reg_form.py', 'a+', encoding="utf-8") as student_s:
+    for lines in range(student_count):
+        student_number = str(input(f"Please enter student's number for attendance register: "))
+        student_s.write(student_number + ":    _________" + "\n")
+print(f" the amount of student numbers input is {student_count}")
+print(student_s)
