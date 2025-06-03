709e830c4c70b470795ef40c546e7035acb9edc5
WernerVal-8485 <wernerval00@gmail.com>
2025-06-03 15:54:16 +0200

    commit Final 20250603

--- Level 1 - Python for Software Engineering/M02T07 – Programming with Built-in Functions/float_manipulation.py
+++ Level 1 - Python for Software Engineering/M02T07 – Programming with Built-in Functions/float_manipulation.py
@@ -1,17 +1,37 @@
+## Auto graded task 1 - M02T07
+
+# Import built-in fucntion statistics
 import statistics
 
+# Create Empty list to store the user input
 user_input = []
+
+# Assign 0 to i for the while loop
 i = 0
 
+# While loop to append empty list
+# Check inputs are less that 10
 while i < 10:
     user_values = float(input("Please give any number (with or without decimal): "))
     user_input.append(user_values)
     i += 1
-    
+
+# Printing the total of the values 
+print(f"The total sum of all 10 values is equal to: {sum(user_input)}")
+
+# Calculate the Maximum
+# Calculate the index of the Maximun
+# Printing the result
 input_maximum = max(user_input)
 maximum_index = user_input.index(input_maximum)
-print(f"The total sum of all 10 values is equal to: {sum(user_input)}")
 print(f"The Maximum value of the 10 inputs is {input_maximum} and it is indexed at {maximum_index}.")
+
+# Printing the minimum with embedded fucntions
+# Creating an embedded min calculation
+# Embedded function for calculating the index of the Minimum value
 print(f"The Minimum value of the 10 inputs is {min(user_input)} and it is indexed at {user_input.index(min(user_input))}")
+
+# Calculating the mean of the input
+# Printing the result and rounding the value to 2 decimals
 mean_input  = statistics.mean(user_input)
-print(f"The Mean for the 10 values, rounded to two decimals, is {round(mean_input,2)}.")
+print(f"The Mean for the 10 values, rounded to two decimals, is: {round(mean_input,2)}.")
