e103e50ca55b066962f049b5763599352dab0445
WernerVal-8485 <wernerval00@gmail.com>
2025-06-03 18:17:25 +0200

    commit

--- Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/conversion.py
+++ Level 1 - Python for Software Engineering/M01T03 – Variables – Storing Data in Programs/conversion.py
@@ -6,7 +6,7 @@ num2 = 23
 num3 = 150
 string1 = "100"
 
-<<<<<<< HEAD
+
 num1 = int(num1)
 num2 = float(num2)
 num3 = str(num3)
@@ -16,9 +16,9 @@ print(num1)
 print(num2)
 print(num3)
 print(string1)
-=======
+
 print(int(num1))
 print(float(num2))
 print(str(num3))
 print(int(string1))
->>>>>>> 06a87b37dc4b599d0c34a54605dc9c1e2153a5ab
+
