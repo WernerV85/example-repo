efb194142b838fe5d0f63a5b7e317b9503cee708
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-06-11 21:57:58 +0200

    Create calc_app.py

---
+++ Level 1 - Python for Software Engineering/M03T01 – Defensive Programming – Exception Handling/calc_app.py
@@ -0,0 +1,21 @@
+
+def additions_input(num1, num2):
+    total_additions = num1 + num2
+    return (total_additions)
+
+def subtraction_input(num1, num2):
+    total_subtration = num1 - num2
+    return (total_subtration)
+
+def multiplication_input(num1, num2):
+    total_multiplication = num1 * num2
+    return (total_multiplication)
+
+def devision_input(num1, num2):
+    if num2 == 0:
+        print("Cannot devide by zero!!!")
+    else:
+        total_devision = num1 / num2
+        return (total_devision)
+    
+    
