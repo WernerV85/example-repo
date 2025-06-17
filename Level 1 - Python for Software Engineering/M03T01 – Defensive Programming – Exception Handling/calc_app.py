aca1ce30fbdbc2bda442f0a41059da158b639bab
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-06-17 18:33:58 +0200

    commit

--- Level 1 - Python for Software Engineering/M03T01 – Defensive Programming – Exception Handling/calc_app.py
+++ Level 1 - Python for Software Engineering/M03T01 – Defensive Programming – Exception Handling/calc_app.py
@@ -4,18 +4,37 @@ def additions_input(num1, num2):
     return (total_additions)
 
 def subtraction_input(num1, num2):
-    total_subtration = num1 - num2
-    return (total_subtration)
+    total_subtraction = num1 - num2
+    return (total_subtraction)
 
 def multiplication_input(num1, num2):
     total_multiplication = num1 * num2
     return (total_multiplication)
 
-def devision_input(num1, num2):
+def division_input(num1, num2):
     if num2 == 0:
-        print("Cannot devide by zero!!!")
+        print("Cannot divide by zero!!!")
     else:
-        total_devision = num1 / num2
-        return (total_devision)
+        total_division = num1 / num2
+        return (total_division)
     
-    
+calculation_select = {1 : "Addition",
+                       2 : "Subtraction",
+                       3 : "Multiplication",
+                       4 : "Division"}
+
+print("Calculation Option: ")
+for key, value in calculation_select.items():
+    print(f"{key} : {value}")
+print(int(input("Select Calculation you want to perform: ")))
+
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number:"))
+
+while True:
+    try:
+        if calculation_select in (1, 2, 3, 4):
+            
+            break
+    except Exception:
+        print("Please supply correct calculation option.")
