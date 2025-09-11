e7313298d5601e5c913d95e673201e99de610039
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-09-11 19:23:29 +0200

    Update minesweeper.py

--- Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
+++ Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
@@ -9,11 +9,13 @@ minesweeper_grid = [["-" , "#", "-", "-", "#"],
 def minesweep_solve (minesweeper_grid, row, kolom):
     row = len(minesweeper_grid)
     kolom = len(minesweeper_grid[row])
-
+    mine_count = 0
+    
     buur_cells = [(-1, -1), (-1, 0), (-1, +1),
-                  (0, -1),        , (0, +1),
+                  (0, -1),           (0, +1),
                   (+1, -1), (+1, 0), (+1, +1)]
 
     for ro in range(row):
         for kol in range(kolom):
+            
             print(f'Board has', {row}, 'and', {kolom})
