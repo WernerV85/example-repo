ac7bf227fddc2f7759ae8f3580e1dc6b899ffa67
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-10-01 20:09:59 +0200

    commit

--- Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
+++ Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
@@ -15,18 +15,22 @@ def minesweep_solve (minesweeper_grid, row, kolom):
                   (0, -1),           (0, +1),
                   (+1, -1), (+1, 0), (+1, +1)]
     
-    
     for ro in range(row):
-        for kol in range(kol):
+        for kol in range(kolom):
             if minesweeper_grid[ro][kol] == "-":
                 mine_count = 0
-                for new_ro in range(buur_cells):
-                    for new_kol in range(buur_cells):
-                        if new_ro == "#" and new_kol == "#":
-                            nnew_row , nnew_kol = ro + new_ro, kol + new_kol
+                for new_ro, new_kol in buur_cells:
+                    nnew_row , nnew_kol = ro + new_ro, kol + new_kol
                     if 0 <= nnew_row < row and 0 <= nnew_kol < kolom:
-                        mine_count += 1
+                        if minesweeper_grid[nnew_row][nnew_kol] == "#":
+                            mine_count += 1
                 minesweeper_grid[ro][kol] = mine_count
 
-                print(mine_count)  
-print(minesweeper_grid)
+                print(mine_count)
+    
+    
+minesweep_solve(minesweeper_grid, 5, 5)
+for row in minesweeper_grid:
+    print(row)  
+# Expected output:
+# [2, '#', 2, 1, '#']   
