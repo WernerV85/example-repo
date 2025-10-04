358a4b029d3efae6733525abaa323daea747e7ce
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-10-04 13:22:55 +0200

    Commit final

--- Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
+++ Level 1 - Python for Software Engineering/M03T04 – Data Structures – 2D Lists/minesweeper.py
@@ -1,20 +1,25 @@
+''' Data Structures - 2D lists
+Calculating the adjacent mines in a minesweeper game'''
 
-
+# Creating the minesweeper grid.
 minesweeper_grid = [["-" , "#", "-", "-", "#"],
                     ["#" , "#", "-", "-", "#"],
                     ["-" , "-", "#", "#", "-"],
                     ["#" , "-", "-", "-", "-"],
                     ["-" , "-", "#", "-", "-"]]
 
+# Defining the function to calculate the adjacent mines in the grid.
 def minesweep_solve (minesweeper_grid, row, kolom):
     row = len(minesweeper_grid)
     kolom = len(minesweeper_grid[0])
     mine_count = 0
     
+    # Listing all the positions to check for adjacent mines.
     buur_cells = [(-1, -1), (-1, 0), (-1, +1),
                   (0, -1),           (0, +1),
                   (+1, -1), (+1, 0), (+1, +1)]
     
+    # Running loops to run through the grid to check adjacent mines
     for ro in range(row):
         for kol in range(kolom):
             if minesweeper_grid[ro][kol] == "-":
@@ -27,9 +32,8 @@ def minesweep_solve (minesweeper_grid, row, kolom):
                 minesweeper_grid[ro][kol] = mine_count
     return minesweeper_grid
     
-    
+# running the function to solve the minesweeper grid    
 minesweep_solve(minesweeper_grid, 5, 5)
 for row in minesweeper_grid:
-    print(row)  
-# Expected output:
-# [2, '#', 2, 1, '#']   
+    # Printing the results row by row.
+    print(row)
