846c57cb7d31f8f34051ef03a033f6d9d330327f
WernerVal-8485 <werner.valentin@imqs.co.za>
2025-11-12 20:29:16 +0200

    Commit sort and search

--- Level 1 - Python for Software Engineering/M03T06 – Sorting and Searching/merge_sort.py
+++ Level 1 - Python for Software Engineering/M03T06 – Sorting and Searching/merge_sort.py
@@ -9,111 +9,112 @@ def merge_sort(items):
     items_length = len(items)
 
     #Create temporary storage for merging
-    #temporary_storage = len(items)
+    temporary_storage = [None] * items_length
 
     # Initialise the size of the subsections to 1
-    size_of_subsections = len(items) // 2
+    size_of_subsections = 1
 
     # Iterate until the size of the subsections is less than the length of the list
     while size_of_subsections < items_length:
         # Iterate over the list in steps of size_of_subsections *2
-        for i in range(items_length, size_of_subsections):
-            # Determine the start and end indices of the two subsections 
+        for i in range(0, items_length, size_of_subsections * 2):
+            # Determine the start and end indices of the two subsections
             # to merge
-            left_start, left__end = i, min(i + size_of_subsections, items_length)
+            first_section_start, first_section_end = i, min(
+                i + size_of_subsections, items_length
+            )
 
-            right_start, right_end = left__end, min(left__end + size_of_subsections, items_length)
+            second_section_start, second_section_end = first_section_end, min(
+                first_section_end + size_of_subsections, items_length
+            )
 
             # Define the section to merge
-            sections = (left_start, left__end), (right_start, right_end)
+            sections = (first_section_start, first_section_end), (
+                second_section_start,
+                second_section_end,
+            )
 
             # Call the merge function to merge the subsections
-            merge(items, sections)
-    
+            merge(items, sections, temporary_storage)
+
         # Double the size of subsections for the next iteration
         size_of_subsections *= 2
-    
+
     # Return the sorted list
+    #print("====> merge_sort items:", items)
     return(items)
 
-def merge(items, sections):
+# Defining get_value function to return the length of a string(integer value)
+def get_value(item):
+    if isinstance(item, str):
+        return len(item)
+    return item
+
+def merge(items, sections, temporary_storage):
+
     # Unpack the sections tuple to get the start and end indices
     # of each section.
-    (left_start, left__end), (right_start, right_end) = sections
-    
+    (first_section_start, first_section_end), (
+        second_section_start,
+        second_section_end) = sections
+
     # Initialise indices for the two section and temporary storage
-    merge_list = []
-    left = left_start
-    right = right_start
+    left_index = first_section_start
+    right_index = second_section_start
     temp_index = 0
-    
 
     # Loop until both section have been fully merged
-    while left < len(left_start) or right < len(right_end):
+    while left_index < first_section_end or right_index < second_section_end:
         # Check if both sections still have elements to compare
-        if left < len(left_start) and right < len(right_end):
+        if left_index < first_section_end and right_index < second_section_end:
             # Compare elements from both sections
-            if items[left] < items[right]:
+            if  get_value(items[left_index]) < get_value(items[right_index]):
                 # Place the smaller element into temporary storage
-                merge_list.append(left_start[left])
-                left += 1
+                temporary_storage[temp_index] = items[left_index]
+                left_index += 1
             else: # items[right_index] <= items[left_index]
-                merge_list.append(right_start[right])
-                right += 1
+                temporary_storage[temp_index] = items[right_index]
+                right_index += 1
             temp_index += 1
 
         # If section 1 still has elements left to merge
-        elif left < len(left__end):
+        elif left_index < first_section_end:
             # Copy remaining elements from section 1 to temporary storage
-            for i in range(left, len(left__end)):
-                merge_list.append(left_start[left])
-                left += 1
+            for i in range(left_index, first_section_end):
+                temporary_storage[temp_index] = items[left_index]
+                left_index += 1
                 temp_index += 1
 
         # If section 2 still has elements left to merge
-        else: # right_index , second_section_end  
+        else: # right_index , second_section_end
             # Copy remaining elements from section 2 to temporary storage
-            for i in range(right, len(right_end)):
-                merge_list.append(right_start[right])
-                right += 1
+            for i in range(right_index, second_section_end):
+                temporary_storage[temp_index] = items[right_index]
+                right_index += 1
                 temp_index += 1
 
     # Copy sorted elements from temporary storage back to the original list
     for i in range(temp_index):
-        items[left_start + i] = merge_list[i]
-    
-    return(merge_list)
+        items[first_section_start + i] = temporary_storage[i]
 
+## First list of words
+## Running merge sort function
+## printing results
 example_list = ['Hi', 'Hello', 'Hey', 'Hola', 'Bonjour', 'Ciao', 'Namaste', 'Salaam', 'Konnichiwa', 'Zdravstvuyte']
-#word_length = [(word, len(word)) for word in example_list]
-#new_list = []
-#new_words = []
+sorted_list = merge_sort(example_list)
+print("\n Sorted List: ", example_list[::-1])
 
-#for word, length in word_length:
-#    new_list.append(length)
-#    sorted_list = merge_sort(new_list) 
-
-#for length in word_length:
-#    new_words = merge_sort(word_length)
-
-
-#print(new_list)          
-#print(new_words[::-1])
-#print("Sorted List:", sorted_list[::-1])
-
-merge_list = merge_sort(example_list)
-print("\n Sorted List:", merge_list[::-1])
-
+## Second list of words
+## Running merge sort function
+## printing results
 example_list2 = ['apple', 'orange', 'banana', 'grape', 'kiwi', 'mango', 'peach', 'pear', 'plum', 'cherry']
 sorted_list2 = merge_sort(example_list2)
 print("\n Sorted List 2:", sorted_list2[::-1])
 
+## Third list of words
+## Running merge sort function
+## printing results
 example_list3 = ['dog', 'cat', 'elephant', 'ant', 'zebra', 'lion', 'tiger', 'bear', 'giraffe', 'monkey']
 sorted_list3 = merge_sort(example_list3)
 print("\n Sorted List 3:", sorted_list3[::-1])
 
-example_list4 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-sorted_list4 = merge_sort(example_list4)
-print("\n Sorted List:", sorted_list4[::-1])
-
- 
